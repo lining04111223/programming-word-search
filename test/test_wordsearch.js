568151adca8b33b96854f1a636089b236ab84220
Ning Li <lining04111223@gmail.com>
2022-09-29 00:26:45 +0000

    last version

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,3 +1,4 @@
+//////////Jaxson Baerg & Ning Li//////////
 const chai = require('chai');
 const assert = chai.assert;
 
@@ -15,7 +16,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +32,126 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'B', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'O', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'B', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'BOB');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present horizontally but backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'B', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'O', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'B', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'N', 'H', 'O', 'J', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'JOHN');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically but upside-down", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'B', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'O', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'N', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'H', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'O', 'E', 'R', 'L'],
+      ['B', 'F', 'N', 'H', 'J', 'Z', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'JOHN');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present diagonally going down", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'B', 'U', 'A', 'L'],
+      ['S', 'L', 'I', 'N', 'O', 'E', 'L', 'D'],
+      ['Y', 'F', 'B', 'F', 'N', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'E', 'H', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'R', 'E', 'R', 'L'],
+      ['B', 'F', 'N', 'H', 'J', 'T', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ALBERT');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present diagonally going up", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'B', 'U', 'A', 'L'],
+      ['S', 'L', 'I', 'N', 'O', 'T', 'L', 'D'],
+      ['Y', 'F', 'Z', 'F', 'R', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'E', 'H', 'V', 'R', 'G'],
+      ['W', 'H', 'B', 'S', 'R', 'E', 'R', 'L'],
+      ['B', 'L', 'N', 'H', 'J', 'T', 'Y', 'B'],
+      ['A', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ALBERT');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present diagonally going down but backwards", function() {
+    const result = wordSearch([
+      ['T', 'W', 'C', 'F', 'B', 'U', 'A', 'L'],
+      ['S', 'R', 'I', 'N', 'O', 'T', 'L', 'D'],
+      ['Y', 'F', 'E', 'F', 'R', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'B', 'H', 'V', 'R', 'G'],
+      ['W', 'H', 'B', 'S', 'L', 'E', 'R', 'L'],
+      ['B', 'L', 'N', 'H', 'J', 'A', 'Y', 'B'],
+      ['A', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ALBERT');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present diagonally going up but backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'B', 'U', 'A', 'L'],
+      ['S', 'L', 'I', 'N', 'O', 'A', 'L', 'D'],
+      ['Y', 'F', 'Z', 'F', 'L', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'B', 'H', 'V', 'R', 'G'],
+      ['W', 'H', 'E', 'S', 'R', 'E', 'R', 'L'],
+      ['B', 'R', 'N', 'H', 'J', 'T', 'Y', 'B'],
+      ['T', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ALBERT');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word array is empty", function() {
+    const result = wordSearch([], 'BOB');
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,81 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//////////Jaxson Baerg & Ning Li//////////
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) return false;
 
-module.exports = wordSearch
+    const horizontalJoin = letters.map(ls => ls.join(""));
+    for (l of horizontalJoin) {
+        if (l.includes(word) || l.split("").reverse().join("").includes(word)) return true;
+    }
+
+    const verticalJoin = transpose(letters).map(ls => ls.join(""));
+    for (l of verticalJoin) {
+        if (l.includes(word) || l.split("").reverse().join("").includes(word)) return true;
+    }
+    
+    let foundWordDown = [];
+    let foundWordUp = [];
+    let foundWordDownBack = [];
+    let foundWordUpBack = [];
+    for (let i = 0; i < letters.length; i++) { // Rows
+        for (let x = 0; x < letters[i].length; x++) { // Letters in row
+
+            for (let j = 0; j < word.length; j++) { // Length of inputted word down
+                try {
+                  foundWordDown.push(letters[i + j][x + j]);
+                } catch(err) {
+                  break;
+                }
+            }
+            for (let j = 0; j < word.length; j++) { // Length of inputted word up
+                try {
+                  foundWordUp.push(letters[i - j][x + j]);
+                } catch(err) {
+                  break;
+                }
+            }
+            for (let j = 0; j < word.length; j++) { // Length of inputted word down backwards
+                try {
+                  foundWordDownBack.push(letters[i - j][x - j]);
+                } catch(err) {
+                  break;
+                }
+            }
+            for (let j = 0; j < word.length; j++) { // Length of inputted word up backwards
+                try {
+                  foundWordUpBack.push(letters[i + j][x - j]);
+                } catch(err) {
+                  break;
+                }
+            }
+
+            if (foundWordDown.join("").includes(word)) return true;
+            foundWordDown = [];
+            if (foundWordUp.join("").includes(word)) return true;
+            foundWordUp = [];
+            if (foundWordDownBack.join("").includes(word)) return true;
+            foundWordDownBack = [];
+            if (foundWordUpBack.join("").includes(word)) return true;
+            foundWordUpBack = [];
+        }
+    }
+
+    return false;
+};
+
+const transpose = function (grid) {
+    let newRows = [];
+    const numRows = grid.length//row
+    const numColumns = grid[0].length//column
+  
+      for (let c = 0; c < numColumns; c++) {
+        let newRow = []
+        for (let r = 0; r < numRows; r++) {
+          const swapped = grid[r][c]
+          newRow.push(swapped)
+        }
+      newRows.push(newRow) 
+      }
+  return newRows
+  };
+
+module.exports = wordSearch;
